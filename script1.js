86c5e54e5b9457e6d1c656cf474177916b5c9ed5
THARA HUOKAING <tharahuokaing@gmail.com>
2026-06-29 23:12:09 +0700

    Change wedding audio source from mnemonic.mp3 to monica.mp3

--- script1.js
+++ script1.js
@@ -6,7 +6,7 @@
  * Audio Engine Configuration
  * Uses the local wedding audio asset path
  */
-const weddingAudioSrc = "mnemonic.mp3";
+const weddingAudioSrc = "monica.mp3";
 
 // Create HTMLAudioElement instance
 const weddingAudio = new Audio(weddingAudioSrc);
