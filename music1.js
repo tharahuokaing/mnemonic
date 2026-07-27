30396a1c0bc180e53bcf2b88299ad510e94409e4
THARA HUOKAING <tharahuokaing@gmail.com>
2026-07-27 17:17:44 +0700

    Fix audio source filename for wedding audio

--- music1.js
+++ music1.js
@@ -6,7 +6,7 @@
  * Audio Engine Configuration
  * Uses the local wedding audio asset path
  */
-const weddingAudioSrc = "monica1.mp3";
+const weddingAudioSrc = "Monica.mp3";
 
 // Create HTMLAudioElement instance
 const weddingAudio = new Audio(weddingAudioSrc);
